82a966cf1333b7c7cf0ac9625f4f8f1f2f28d256
DaniYeCa <daniel.dance1912@gmail.com>
2023-09-16 15:11:39 +0200

    change

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,19 +8,19 @@ export const Home = () => {
         <div class="content">
             <h1>Daniel Yepes Carrillo</h1>
             <h4>Estudiante para Full Stack Developer</h4>
-            <a href="public/assets/full stack.pdf" target="_blank">
+            <a href="./assets/full stack.pdf" target="_blank">
             <button>Descarga mi CV</button>
             </a>
                 <div class="icons">
-                    <a href="https://github.com/DaniYeCa" target="_blank"><img src="public/assets/github.png" alt="logo github"></a>
-                    <a href="https://www.linkedin.com/in/daniel-yepes-carrillo/" target="_blank"><img src="public/assets/linkedin.png" alt="logo linkedin"></a>
+                    <a href="https://github.com/DaniYeCa" target="_blank"><img src="./assets/github.png" alt="logo github"></a>
+                    <a href="https://www.linkedin.com/in/daniel-yepes-carrillo/" target="_blank"><img src="./assets/linkedin.png" alt="logo linkedin"></a>
                 </div>
         </div>
                 <div class="hero-image">
-                    <img src="public/assets/avatar.jpg" alt="Avatar" class="avatar">
-                    <span><img src="public/assets/html.png" alt=""></span>
-                    <span><img src="public/assets/css-3.png" alt=""></span>
-                    <span><img src="public/assets/javascript.png" alt=""></span>
+                    <img src="./assets/avatar.jpg" alt="Avatar" class="avatar">
+                    <span><img src="./assets/html.png" alt="html"></span>
+                    <span><img src="./assets/css-3.png" alt="css"></span>
+                    <span><img src="./assets/javascript.png" alt="JavaScript"></span>
                 </div>
     </section>
     `;
